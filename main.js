39d1f63d33cab50d5a297294bb79ea2467244fc7
Santosh kumar gupta <sainkee1997@gmail.com>
2024-07-10 08:09:24 +0530

    midddlewere added

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public"));
+app.use(express.json());
 app.set("view engine", "ejs");
 let selectedCoin = "bitcoin";
 
